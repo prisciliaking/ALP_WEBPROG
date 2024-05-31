b8e35f7926aecc0b540b9b90d41d29d0fe65ed53
Priscilia King <148315234+prisciliaking@users.noreply.github.com>
2024-05-31 23:07:31 +0700

    nyambung role user bisa delete profile

--- ALP_Webprog/profile.php
+++ ALP_Webprog/profile.php
@@ -21,46 +21,55 @@
                     <!-- edit profile name -->
                     <div>
                         <div class="px-4 py-6">
-                            <!-- ini buat update username dan password user -->
-                        <?php if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["action"]) && $_POST["action"] == "updateProfile") {
-                                updateUser();
-                        } ?>
-                            <!-- end -->
-                            
-                        <?php if(isset($_SESSION["username"])){ ?>
-                            <form action="" method="POST">
-                                <input type="hidden" name="action" value="updateProfile">
-                                <div class="md:flex md:flex-col md:items-center md:space-y-6 md:px-52">
-                                    <div class="pb-4 w-full space-y-2">
-                                        <label for="username" class="text-lg">Username</label>
-                                        <input class="rounded-md w-full p-2 md:p-4 text-lg" type="text" name="username" value="<?=$_SESSION["username"]?>">
+                                <!-- ini buat update username dan password user -->
+                            <?php if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["action"]) && $_POST["action"] == "updateProfile") {
+                                    updateUser();
+                            } ?>
+                                <!-- end -->
+                        
+                            <?php if(isset($_SESSION["username"])){ ?>
+                                <form action="" method="POST">
+                                    <input type="hidden" name="action" value="updateProfile">
+                                    <div class="md:flex md:flex-col md:items-center md:space-y-6 md:px-52">
+                                        <div class="pb-4 w-full space-y-2">
+                                            <label for="username" class="text-lg">Username</label>
+                                            <input class="rounded-md w-full p-2 md:p-4 text-lg" type="text" name="username" value="<?=$_SESSION["username"]?>">
+                                        </div>
+                                        <div class="pb-4 w-full space-y-2">
+                                            <label for="password" class="text-lg">Password</label>
+                                            <input class="rounded-md w-full p-2 md:p-4 text-lg" type="text" name="password" value="<?=$_SESSION["password"]?>">
+                                        </div>
                                     </div>
-                                    <div class="pb-4 w-full space-y-2">
-                                        <label for="password" class="text-lg">Password</label>
-                                        <input class="rounded-md w-full p-2 md:p-4 text-lg" type="text" name="password" value="<?=$_SESSION["password"]?>">
+                                    <div class="pt-4 w-full md:px-52 flex justify-center font-semibold md:justify-end space-x-4">
+                                        <input type="submit" value="Update Profile" class="rounded-md p-2 text-lg text-white bg-[#8A6791] hover:bg-[#BEAECB]">
+                                        <button type="button" onclick="window.location.href='logout.php';" class="rounded-md p-2 text-lg text-white bg-[#8A6791] hover:bg-[#BEAECB]">
+                                            Logout
+                                        </button>
+                                    </div>
+                                </form>
+                            <?php } ?>
+
+                            <!-- for see profile list  -->
+                            <?php if ($_SESSION["role"] == "admin") { ?>
+                                <div class="flex md:justify-end">
+                                    <div class="pt-4 w-full md:px-52 flex justify-center font-semibold md:justify-end space-x-4">
+                                        <button type="button" onclick="window.location.href='userlist.php';" class="rounded-md p-2 text-lg text-white bg-[#8A6791] hover:bg-[#BEAECB]">
+                                            User List
+                                        </button>
                                     </div>
                                 </div>
+                            <?php } else { ?>
                                 <div class="pt-4 w-full md:px-52 flex justify-center font-semibold md:justify-end space-x-4">
-                                    <input type="submit" value="Update Profile" class="rounded-md p-2 text-lg text-white bg-[#8A6791] hover:bg-[#BEAECB]">
-                                    <button type="button" onclick="window.location.href='logout.php';" class="rounded-md p-2 text-lg text-white bg-[#8A6791] hover:bg-[#BEAECB]">
-                                        Logout
-                                    </button>
+                                    <form method="POST" action="deleteuser.php">
+                                        <input type="hidden" name="deleteUser" value="1">
+                                        <button type="submit" class="rounded-md p-2 text-lg text-white bg-[#8A6791] hover:bg-[#BEAECB]">
+                                            Delete Profile
+                                        </button>
+                                    </form>
                                 </div>
-                            </form>
-                        <?php } ?>
+                            <?php } ?>
 
                             
-                            <!-- for add product -->
-                            <?php if($_SESSION["role"] == "admin") { ?>
-                            <div class="md:px-52 flex justify-center md:justify-end pt-6 ">
-                                <a href="addproduct.php">
-                                    <button
-                                        class=" bg-[#8A6791] text-lg text-white font-semibold py-2 px-4 rounded hover:bg-[#BEAECB] focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
-                                        Add Product
-                                    </button>
-                                </a>
-                            </div>
-                            <?php } ?>
                         </div>
 
                     </div>
